e3febd004c0b1fd60287263da400e664e466a403
Raptor8532 <23257517+Raptor8532@users.noreply.github.com>
2023-11-13 13:25:42 +0100

    terraform and ansible k3s config

--- k3s/main.tf
+++ k3s/main.tf
@@ -11,25 +11,29 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-// variables that can be overriden
-variable "hostname" { default = "test" }
-variable "domain" { default = "example.com" }
-variable "ip_type" { default = "dhcp" } # dhcp is other valid type
-variable "memoryMB" { default = 1024 * 1 }
-variable "cpu" { default = 1 }
-variable "diskBytes" { default = 1024*1024*1024*5 }
-variable "private_key_path" {
-  description = "Path to the private SSH key, used to access the instance."
-  default     = "~/.ssh/id_rsa"
-}
+############################################################
+//-> Config Storage
+############################################################
 
 // fetch the latest ubuntu release image from their mirrors
 resource "libvirt_volume" "os_image" {
   name   = "${var.hostname}-os_image"
   pool   = "default"
-  source = "jammy-server-cloudimg-amd64.img"
+  source = "./jammy-server-cloudimg-amd64.img"
   format = "qcow2"
 }
+
+// Resize qcow2 
+resource "libvirt_volume" "disk_resized" {
+  name           = "${var.hostname}-disk"
+  base_volume_id = libvirt_volume.os_image.id
+  pool           = "default"
+  size           = 1024 * 1024 * 1024 * var.diskGB
+}
+
+############################################################
+//-> Config cloud init
+############################################################
 
 // Use CloudInit ISO to add ssh-key to the instance
 resource "libvirt_cloudinit_disk" "commoninit" {
@@ -64,37 +68,17 @@ data "template_file" "network_config" {
 }
 
 ############################################################
-//-> Create storage
-############################################################
-
-resource "libvirt_pool" "k3s_pool" {
-  name = "cluster"
-  type = "dir"
-  path = "/home/deltaro/vm-storage/k3s"
-}
-
-resource "libvirt_volume" "master" {
-  name = "master-k3s.qcow2"
-  pool   = "default"
-  size = var.diskBytes
-}
-
-
-############################################################
 //-> Create the machine
 ############################################################
 resource "libvirt_domain" "domain-ubuntu" {
   # domain name in libvirt, not hostname
   name   = var.hostname
-  memory = var.memoryMB
+  memory = 1024 * var.ramGB
   vcpu   = var.cpu
   #qemu_agent = true
 
   disk {
-    volume_id = libvirt_volume.os_image.id
-  }
-  disk {
-    volume_id = libvirt_volume.master.id
+    volume_id = libvirt_volume.disk_resized.id
   }
 
   network_interface {
@@ -133,7 +117,7 @@ resource "libvirt_domain" "domain-ubuntu" {
   }
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = "ANSIBLE_HOST_KEY_CHECKING=False /home/deltaro/.local/bin/ansible-playbook -u ubuntu -i ${self.network_interface.0.addresses.0}, --private-key ${var.private_key_path} provision.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i ${self.network_interface.0.addresses.0}, -e k3s_version=v1.26.9+k3s1 -e master_ip=${self.network_interface.0.addresses.0} --private-key ${var.private_key_path} k3s-ansible/site.yml"
   }
 }
 
